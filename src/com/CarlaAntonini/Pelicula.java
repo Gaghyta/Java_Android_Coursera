281ee3fec7a084628dc7d2e1b5994476fdd77c90
Carla Antonini <carla.antonini@hotmail.com>
2024-01-08 16:58:22 -0300

    Agrego estructura de proyecto y clase Pelicula con atributos

---
+++ src/com/CarlaAntonini/Pelicula.java
@@ -0,0 +1,13 @@
+package com.CarlaAntonini;
+
+public class Pelicula {
+    // Atributos
+    private String tituloPelicula;
+    private String generoPelicula;
+    private String creadorPelicula;
+    private int añoDeEstreno;
+    private int duracionPelicula;
+    private boolean visto;
+
+
+}
