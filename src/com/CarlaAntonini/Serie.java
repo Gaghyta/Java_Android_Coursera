e5bd4881f3517029e2840421b0133d03cca1a417
Carla Antonini <carla.antonini@hotmail.com>
2024-01-08 17:32:41 -0300

    Subo clase Serie y sus atributos

---
+++ src/com/CarlaAntonini/Serie.java
@@ -0,0 +1,11 @@
+package com.CarlaAntonini;
+
+public class Serie {
+
+    private String tituloSerie;
+    private int numeroDeTemporadas;
+    private boolean visto;
+    private String generoSerie;
+    private String creadorSerie;
+    private int duracionSerie;
+}
