2b5d7204be6f795ef3937f60bf234e8cff20aea6
Carla Antonini <carla.antonini@hotmail.com>
2024-01-08 17:34:13 -0300

    Subo Constructores pedidos para Serie

--- src/com/CarlaAntonini/Serie.java
+++ src/com/CarlaAntonini/Serie.java
@@ -8,4 +8,33 @@ public class Serie {
     private String generoSerie;
     private String creadorSerie;
     private int duracionSerie;
+
+
+
+    // CONSTRUCTORES
+    // CONSTRUCTOR DEFAULT
+    public Serie() {
+        this.numeroDeTemporadas = 1;
+        this.visto = false;
+        // Valores default según el tipo de dato
+        this.tituloSerie = "";
+        this.generoSerie = "";
+        this.creadorSerie = "";
+        this.duracionSerie = 0;
+    }
+
+    // CONSTRUCTOR CON TITULO Y CREADOR
+    public Serie(String tituloSerie, String creadorSerie) {
+        this(); // Llama al constructor default
+        this.tituloSerie = tituloSerie;
+        this.creadorSerie = creadorSerie;
+    }
+
+    // Constructor con todos los atributos excepto visto
+    public Serie(String tituloSerie, int numeroDeTemporadas, String generoSerie, String creadorSerie, int duracionSerie) {
+        this(tituloSerie, creadorSerie); // Llama al constructor con titulo y creador para inicializar esos atributos
+        this.numeroDeTemporadas = numeroDeTemporadas;
+        this.generoSerie = generoSerie;
+        this.duracionSerie = duracionSerie;
+    }
 }
